f49b9b4516b683b340c39a6abd96c030fbf7116e
Leo Gaunt <36968548+LeoGaunt@users.noreply.github.com>
2026-04-04 16:02:02 +0100

    Changed dependencies

--- colab_setup.sh
+++ colab_setup.sh
@@ -3,8 +3,8 @@ pip install open_clip_torch
 pip install git+https://github.com/modestyachts/ImageNetV2_pytorch
 pip install kornia
 echo "Installing Grad-CAM dependencies"
-pip install git+https://github.com/openai/CLIP.git
-pip install torchray
+#pip install git+https://github.com/openai/CLIP.git
+#pip install torchray
 echo "Setting up datasets"
 mkdir datasets
 cd datasets
